14d388860fd8fb043f15186908ef72358c4b8fbd
Malek Salem <malek.salem.14@gmail.com>
2020-06-15 13:33:44 +0000

    add initial files

---
+++ sort.h
@@ -0,0 +1,23 @@
+#ifndef __HEADER__
+#define __HEADER__
+
+#include<stdlib.h>
+#include<stdio.h>
+
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+}
+
+
+#endif
