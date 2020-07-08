39496ebf450e790e66c687e8013495d80097b647
Malek Salem <malek.salem.14@gmail.com>
2020-07-08 08:51:49 +0000

    fix code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,22 +11,25 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t idx, current_n;
-	int temp_variable;
+	int temp_variable, smallest_number;
 
 	if (!array)
-		return ;
+		return;
 
-	for (idx = 0; idx < size  - 1; idx++)
+	for (idx = 0; idx < size; idx++)
 	{
+		smallest_number = idx;
 		for (current_n = idx + 1; current_n < size; current_n++)
 		{
-			if (array[idx] > array[current_n])
-			{
-				temp_variable = array[idx];
-				array[idx] = array[current_n];
-				array[current_n] = temp_variable;
-			}
+			if (array[current_n] < array[smallest_number])
+				smallest_number = current_n;
 		}
-		print_array(array, size);
+		if (array[idx] > array[smallest_number])
+		{
+			temp_variable = array[idx];
+			array[idx] = array[smallest_number];
+			array[smallest_number] = temp_variable;
+			print_array(array, size);
+		}
 	}
 }
